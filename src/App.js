25a0217651197992ca81e9201b5248dd005c92e4
MrRabotnik <sargis2014@gmail.com>
2020-09-04 04:08:34 +0400

    Currency Problem Solved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
-import Header from './modules/Header';
-import Body from './modules/Body';
-import Footer from './modules/Footer';
+import React, { Component } from 'react';
+import "./App.css"
 
 
+export default class Currency extends Component {
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Body />
-			<Footer />
-		</div>
-	);
+    state = {
+        price: 15,
+        currency: "$",
+    };
+
+    currencyChange() {
+        if (this.state.currency === "$") {
+            this.setState({
+                price: this.state.price * 478,
+                currency: "dram",
+            })
+            this.price = `${this.state.price * 487} Դրամ`
+
+        } else if (this.state.currency === "dram") {
+            this.setState({
+                price: this.state.price / 478,
+                currency: "$",
+            })
+            console.log(this.state.price, this.state.currency)
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => {this.currencyChange()}}>Chenge Currency</button>
+                <span>{this.state.price} {this.state.currency}</span>
+            </div>
+        );
+    };
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import Currency from './App';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Currency />,
+    document.getElementById('root')
 );
-
