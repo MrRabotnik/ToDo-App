82c848b5341abe4c93c2bc5c69adc7a7a97bf5ed
MrRabotnik <sargis2014@gmail.com>
2020-09-07 03:35:46 +0400

    Almost ready...bug in saveValue method

--- src/App.js
+++ src/App.js
@@ -13,36 +13,60 @@ export default class Calculator extends Component {
     state = {
         value: "",
         operand1: "",
-        operand2: "0",
+        operand2: 0,
         valueChange:false,
         operator: "",
+        firstOperand: true,
     }
 
     changeValue(num) {
-        this.setState(() => this.state.value += num)
+        this.setState({
+            value : this.state.value + num
+        })
     }
 
-    saveValue(operator){
-        this.setState(() =>{
-            return {
-                this.state.operand1 = this.state.value,
-                this.state.operator = operator,
-                this.state.value = `${this.state.operand1}${this.state.operator}${this.state.operand2}`,
-            }
-        })
+    saveValue(operator) {
+        if (operator === "+" || operator === "-" || operator === "*" || operator === "/") {
+            this.setState({
+                operand1: this.state.value,
+                operator: operator,
+                operand2: this.state.firstOperand ? this.state.operand1 : eval(`${this.state.operand2} ${this.state.operator} ${this.state.operand1}`),
+                value: '',
+                firstOperand: false,
+            },console.log(this.state))
+        } else if (operator === "=") {
+            this.setState({
+                value: this.state.operand2,
+                operand1: "",
+                operator: "",
+                firstOperand: true,
+            })
+        } else if (operator === "<") {
+            this.setState({
+                value: this.state.value.slice(0, this.state.value.length - 1),
+            })
+        } else {
+            this.setState({
+                value: "",
+                operand1: "",
+                operand2: 0,
+                operator: "",
+                firstOperand: true,
+            })
+        }
     }
 
     createNumberButtons(){
         for (let i = 1; i <= 9; i++) {
             let id = `btn_number_${i}`
-            this.numberBtns.push(<button onClick={() => { this.changeValue(i) }} className="number-btns" id={id}>{i}</button>)
+            this.numberBtns.push(<button key={i} onClick={() => { this.changeValue(i) }} className="number-btns" id={id}>{i}</button>)
         }
     }
 
     createOperatorButtons() {
         for (let i of this.operators) {
             let id = `operator_btn_${i}`
-            this.operatorBtns.push(<button onClick={() => { this.saveValue(i) }} className="operator-btns" id={id}>{i}</button>)
+            this.operatorBtns.push(<button key={i} onClick={() => { this.saveValue(i) }} className="operator-btns" id={id}>{i}</button>)
         }
     }
 
@@ -52,7 +76,7 @@ export default class Calculator extends Component {
 
         return ( 
             <div className = "calculator" >
-                <input onChange = {()=>{} } value = { this.state.value } placeholder = "0" />
+                <input onChange = {()=>{} } value = { this.state.value } placeholder = "Enter a Number" />
                 { this.numberBtns }
                 <button onClick={() => { this.changeValue(0) }} className = "number-btns" id = "btn_number_0" > 0 </button>
                 { this.operatorBtns }
